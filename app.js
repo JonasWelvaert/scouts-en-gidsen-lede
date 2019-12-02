bc9e183909c70510cf29fede0532b19600f81ac4
JonasWelvaert <58304396+JonasWelvaert@users.noreply.github.com>
2019-12-02 18:36:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://Jonas:LocalLibrary@cluster0-mzd3j.azure.mongodb.net/ScoutsEnGidsenLede?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://Jonas:LocalLibrary@cluster0-mzd3j.azure.mongodb.net/ScoutsEnGidsenLede?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
